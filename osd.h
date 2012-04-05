57a0f356445847568183de7f01fce546175fb4be
Lars Hanisch <dvb@flensrocker.de>
2012-04-05 13:01:22 +0200

    add rule for moc-files

--- osd.h
+++ osd.h
@@ -15,7 +15,7 @@ namespace dbus2vdr
   private:
     QDBusInterface *interface;
 
-  public Q_SLOTS:
+  public slots:
     void Open(QString osdid, int left, int top);
     void Display(QString filename, int left, int top, int vx, int vy);
     void Close(QString osdid);
